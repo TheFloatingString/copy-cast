681aef66f240e75f5f45e7fa814e01d116dcefd2
The Floating String <laurencel2001@gmail.com>
2022-10-01 20:20:33 -0400

    Add web socket WIP

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+"""
+Tried using Flask... issues with websockets...
+Will try Fast API instead.
+"""
+
+import socketserver
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+@app.route("/test")
+def home():
+    return "Welcome to repli.cate's Rest API!", 200
+
+@socketio.on("test_socket", namespace="/test_socket")
+def test_socket(data):
+    print(f"from client: {data}")
+    emit("response", {"from": "server"})
+
 if __name__ == "__main__":
-    socketio.run(app)
+    # app.run(port=8080, debug=True)
+    socketio.run(app, host="127.0.0.1", port=8080, debug=True)
